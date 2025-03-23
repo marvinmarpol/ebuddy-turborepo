c0d828f7eaddd4b1a2200e48cd824bc41b11d825
marvinmarpol <marvin.mitchell@amarbank.co.id>
2025-03-24 01:04:28 +0700

    chore(next): initialize basic app nextjs app

---
+++ apps/frontend-repo/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return <Button appName='frontend-repo'>Click me</Button>;
+}
