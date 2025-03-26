4a3bcfba5f921963daa01bc56acd728a21a784b9
unknown <marvinmarpol@gmail.com>
2025-03-26 18:19:48 +0700

    chore(theme): change basic color theme

--- apps/frontend-repo/theme/default.ts
+++ apps/frontend-repo/theme/default.ts
@@ -3,6 +3,11 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   cssVariables: true,
+  palette: {
+    primary: {
+      main: '#2e7d32',
+    },
+  },
   typography: {
     fontFamily: 'var(--font-geist-sans)',
   },
