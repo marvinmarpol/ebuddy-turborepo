914cc79e295fe29f86bac6a78db1b6a6a6b0bfc2
unknown <marvinmarpol@gmail.com>
2025-03-26 12:28:06 +0700

    chore(emulator): change env name for firestore emulator

--- apps/backend-repo/src/config/firebaseConfig.ts
+++ apps/backend-repo/src/config/firebaseConfig.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import admin from 'firebase-admin';
 
 const nodeEnv = process.env.NODE_ENV || 'development';
-const firestoreEmulatorHost = process.env.FIRESTORE_HOST || '127.0.0.1:8080';
+const firestoreEmulatorHost = process.env.FIRESTORE_EMULATOR_HOST || '127.0.0.1:8080';
 const serviceAccount = (process.env.FIREBASE_ACCOUNT_PATH as string) || '';
 
 if (serviceAccount) {
